4d9ebb0a02585e71ea26c09caa1fff6918cb746d
Piotr Matras <piotrmatras21@gmail.com>
2021-11-24 17:32:12 +0100

    added bootstrapping embedded HTTP server on port configured in .env, logging server url

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,14 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const serverPort = app.get(ConfigService).get<number>('server.port');
+  app.setGlobalPrefix('/api');
+  await app.listen(serverPort);
+  const serverUrl = await app.getUrl();
+  new Logger('ServerBootstrap').log(`Server listening on ${serverUrl}`);
 }
 bootstrap();
