1a92d9391e986ed9b095bdb395fa439ef768b078
Piotr Matras <piotrmatras21@gmail.com>
2021-11-25 12:08:23 +0100

    MongoDB connection initialization implemented

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,15 +1,30 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { Logger, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { getConfig } from './config/configuration';
+import { getConfig, AppConfig } from './config/configuration';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [getConfig],
       cache: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const { username, password, host, port, name } =
+          configService.get<AppConfig['database']>('database');
+        new Logger('MongoConnection').log(
+          `Initializing connection with ${name} database on ${host}:${port}`,
+        );
+        return {
+          uri: `mongodb://${username}:${password}@${host}:${port}/${name}?ssl=false`,
+        };
+      },
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
