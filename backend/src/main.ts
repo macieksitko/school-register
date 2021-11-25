59d1094551612111b81a6472d865bac894049d51
maixdu <mmacieksitko@gmail.com>
2021-11-25 19:28:42 +0100

    feat: add swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,11 +2,24 @@ import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const serverPort = app.get(ConfigService).get<number>('server.port');
   app.setGlobalPrefix('/api');
+
+  /** Setup Swagger */
+  const config = new DocumentBuilder()
+    .setTitle('School registry')
+    .setDescription('School registry API description')
+    .setVersion('1.0')
+    .addTag('school-registry')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(serverPort);
   const serverUrl = await app.getUrl();
   new Logger('ServerBootstrap').log(`Server listening on ${serverUrl}`);
