98cb3a8633703cc9018c3df9da1209c99de225ed
Piotr Matras <piotrmatras21@gmail.com>
2021-11-25 11:59:22 +0100

    initializing mongodb with user for new db, updated .env config and config loading

--- backend/src/config/configuration.ts
+++ backend/src/config/configuration.ts
@@ -2,11 +2,25 @@ interface AppConfig {
   server: {
     port: number;
   };
+  database: {
+    host: string;
+    port: number;
+    username: string;
+    password: string;
+    name: string;
+  };
 }
 
 const getConfig = (): AppConfig => ({
   server: {
     port: parseInt(process.env.SERVER_PORT, 10) || 3000,
+  },
+  database: {
+    host: process.env.DB_HOST || '127.0.0.1',
+    port: parseInt(process.env.DB_PORT, 10) || 27017,
+    username: process.env.DB_USERNAME || 'admin',
+    password: process.env.DB_PASSWORD || 'admin',
+    name: process.env.DB_NAME || 'school_register',
   },
 });
 
