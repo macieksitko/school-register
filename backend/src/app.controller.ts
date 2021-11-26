218dc185eca3ef69de1ee2fe9b7ff35adb3daad0
Piotr Matras <piotrmatras21@gmail.com>
2021-11-26 21:23:39 +0100

    implemented RBAC authorization, defined 'Admin' and 'Teacher' roles (#2)

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Post, UseGuards, Request, Get } from '@nestjs/common';
 import { AuthService } from './auth/auth.service';
 import { Public } from './auth/decorators/public.decorator';
+import { Roles } from './auth/decorators/roles.decorator';
 import { LocalAuthGuard } from './auth/guards/local.guard';
+import { Role } from './auth/roles/role.enum';
 @Controller('auth')
 export class AppController {
   constructor(private authService: AuthService) {}
@@ -13,6 +15,7 @@ export class AppController {
     return this.authService.login(req.user);
   }
 
+  @Roles(Role.Admin)
   @Get('profile')
   getProfile(@Request() req) {
     return req.user;
