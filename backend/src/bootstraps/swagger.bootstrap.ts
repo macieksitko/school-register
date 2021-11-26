959017d79c828cb6b2a82a1778ccf3bb96e0c362
Piotr Matras <piotrmatras21@gmail.com>
2021-11-26 14:13:22 +0100

    updated Swagger docs config

--- backend/src/bootstraps/swagger.bootstrap.ts
+++ backend/src/bootstraps/swagger.bootstrap.ts
@@ -3,10 +3,14 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 export default (app: NestExpressApplication) => {
   const config = new DocumentBuilder()
-    .setTitle('School registry')
-    .setDescription('School registry API description')
+    .setTitle('REST API Docs')
+    .setDescription('School Register REST API Documentation')
+    .setVersion('0.0.1')
+    .addTag('school_register')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
+  SwaggerModule.setup('api/docs', app, document, {
+    customSiteTitle: 'School Register API Docs',
+  });
 };
