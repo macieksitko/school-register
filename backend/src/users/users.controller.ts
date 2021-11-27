357605be428df47a25344b07744020d206315b9d
Piotr Matras <piotrmatras21@gmail.com>
2021-11-27 16:08:27 +0100

    implemented new users creation by admin users

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,13 +1,52 @@
-import { Body, Controller, Get, Post, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Logger,
+  Req,
+  BadRequestException,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { Role } from 'src/auth/roles/role.enum';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  private readonly logger = new Logger(UsersController.name);
   constructor(private readonly usersService: UsersService) {}
 
-  @Post()
-  async create(@Body() createUserDto: CreateUserDto) {
-    await this.usersService.create(createUserDto);
+  @Roles(Role.Admin)
+  @Post('create')
+  async create(@Req() req: any, @Body() createUserDto: CreateUserDto) {
+    const {
+      user: { username: creator, userId: creatorId },
+    } = req;
+    const { username, role } = createUserDto;
+
+    this.logger.log(
+      `User ${creator} is trying to create new user ${username} with role ${role}`,
+    );
+
+    const userExists = await this.usersService.userExists(username);
+    if (userExists) {
+      const msg = `Failed to create user - user ${username} already exists`;
+      this.logger.error(msg);
+      throw new BadRequestException(msg);
+    }
+
+    const createdUser = await this.usersService.create(
+      createUserDto,
+      creatorId,
+    );
+
+    if (createdUser) {
+      this.logger.log(`User ${username} created successfully`);
+      return { message: `User ${username} created successfully` };
+    }
+
+    this.logger.error(`Failed to create user ${username}`);
+    return { error: `Failed to create user ${createUserDto}` };
   }
 }
