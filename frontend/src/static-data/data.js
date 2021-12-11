d41e0b9e80fb4618ea4db12c7dad7b50f2ddb123
Pawel Nowak <pawelnowak885@gmail.com>
2021-12-12 00:35:45 +0100

    Added mark-sheet frontend prototype

---
+++ frontend/src/static-data/data.js
@@ -0,0 +1,74 @@
+export const grades = [
+    {
+        "ID": 1,
+        "subject": "Biology",
+        "term1Grades": [
+            {
+                "grade": 5,
+                // reszte informacji o ocenie można wyświetlać w tooltipie, ale nwm czy nam to potrzebne
+                // można dorzucić jeszcze nauczyciela, kategorie, czyliczyćdośredniej
+                "weight": 3,
+                "comment": "comment about grade?",
+                "date": "1964/03/16"
+            },
+            {
+                "grade": 4.5,
+                "weight": 2,
+                "comment": "comment about grade?",
+                "date": "1968/03/16"
+            }
+        ],
+        "term2Grades": [
+            {
+                "grade": 5,
+                "weight": 2,
+                "comment": "comment about grade?",
+                "date": "1968/03/16"
+            }
+        ],
+        "term1Grade": 5,
+        "term1Avg": 4.78,
+        "term2Grade": 5,
+        "term2Avg": 5,
+        "finalAvg": 4.70,
+        "finalGrade": 5
+    },
+    {
+        "ID": 1,
+        "subject": "Math",
+        "term1Grades": [
+            {
+                "grade": 4,
+                "weight": 3,
+                "comment": "comment about grade?",
+                "date": "1964/03/16"
+                // tutaj można dorzucić jeszcze autora, kategorie, datę, czy liczyć do średniej
+            },
+            {
+                "grade": 5,
+                "weight": 2,
+                "comment": "comment about grade?",
+                "date": "1968/03/16"
+            },
+            {
+                "grade": 3.75,
+                "weight": 2,
+                "comment": "comment about grade?",
+                "date": "1968/03/16"
+            }
+            ,
+            {
+                "grade": "np",
+                "weight": null,
+                "comment": "comment about grade?",
+                "date": "1968/03/16"
+            }
+        ],
+        "term2Grades": [],
+        "term1Grade": 4,
+        "term1Avg": 4.28,
+        "term2Grade": 4.5,
+        "term2Avg": 4.35,
+        "finalAvg": 4.20,
+        "finalGrade": 5
+    }];
