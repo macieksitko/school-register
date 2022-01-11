78b3fd9e0d51f4e920dd99df93ff70c248ec81b0
mwalasz <mwalasz@interia.pl>
2022-01-11 08:37:55 +0100

    Added popup for new mark

--- frontend/src/pages/mark-sheet/mark-sheet.js
+++ frontend/src/pages/mark-sheet/mark-sheet.js
@@ -8,12 +8,14 @@ import MarkSheetGrid from "../../components/mark-sheet-grid/mark-sheet-grid";
 import SelectBox from "devextreme-react/select-box";
 import { Button } from "devextreme-react/button";
 import "./mark-sheet.scss";
+import MarkPopup from "../../components/mark-popup/mark-popup";
 
 export default function MarkSheet() {
   const [subjects, setSubjects] = useState(mockedSubjects.map((x) => x.name));
   const [students, setStudents] = useState(mockedStudents.map((x) => x.name));
   const [selectedSubject, setSelectedSubject] = useState("");
   const [selectedStudent, setSelectedStudent] = useState("");
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
 
   const allSelected = selectedStudent !== "" && selectedSubject !== "";
 
@@ -21,7 +23,13 @@ export default function MarkSheet() {
     <React.Fragment>
       <div className="header">
         <h2 className={"content-block"}>Mark-Sheet</h2>
-        <Button text="Add mark" disabled={!allSelected} type="normal" className="content-block" />
+        <Button
+          text="Add mark"
+          disabled={!allSelected}
+          type="normal"
+          className="content-block"
+          onClick={() => setIsPopupVisible(true)}
+        />
       </div>
       <div className="selectContainer">
         <SelectBox
@@ -41,6 +49,15 @@ export default function MarkSheet() {
         />
       </div>
       {allSelected && <MarkSheetGrid grades={grades} />}
+      <MarkPopup
+        studentName={selectedStudent}
+        isVisible={isPopupVisible}
+        onClose={() => setIsPopupVisible(false)}
+        onSave={(data) => {
+          window.alert(JSON.stringify(data));
+          setIsPopupVisible(false);
+        }}
+      />
     </React.Fragment>
   );
 }
