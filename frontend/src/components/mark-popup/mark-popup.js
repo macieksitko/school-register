3b99a750d43f841c6b7e4f3456274e9a177f8b7e
mwalasz <mwalasz@interia.pl>
2022-01-11 08:39:10 +0100

    Added popup for new mark

---
+++ frontend/src/components/mark-popup/mark-popup.js
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import { Popup, ToolbarItem } from "devextreme-react/popup";
+import { NumberBox } from "devextreme-react/number-box";
+import RadioGroup from "devextreme-react/radio-group";
+import TextArea from "devextreme-react/text-area";
+import "./mark-popup.scss";
+
+export default function MarkPopup({ isVisible, onClose, onSave, studentName }) {
+  const defaultMark = { mark: 3, weight: 1, comment: "" };
+  const [markData, setMarkData] = useState(defaultMark);
+
+  const closeButton = {
+    icon: "close",
+    text: "Close",
+    onClick: () => {
+      onClose();
+      setMarkData(defaultMark);
+    },
+  };
+
+  const saveButton = {
+    icon: "check",
+    text: "Save",
+    onClick: () => {
+      onSave(markData);
+      setMarkData(defaultMark);
+    },
+  };
+
+  const tests = [
+    { id: 0, text: "Activity" },
+    { id: 1, text: "Test" },
+    { id: 2, text: "Exam" },
+  ];
+
+  return (
+    <Popup
+      visible={isVisible}
+      onHiding={onClose}
+      dragEnabled={false}
+      closeOnOutsideClick
+      showCloseButton
+      showTitle
+      title="Add new mark"
+      container=".dx-viewport"
+      className="popup"
+      width={"35vw"}
+    >
+      <ToolbarItem widget="dxButton" toolbar="bottom" location="before" options={saveButton} />
+      <ToolbarItem widget="dxButton" toolbar="bottom" location="after" options={closeButton} />
+      <div className="form">
+        <div className="dx-fieldset">
+          <div className="dx-field">
+            <div className="dx-field-label">Student's name</div>
+            <div className="dx-field-value">{studentName}</div>
+          </div>
+          <div className="dx-field">
+            <div className="dx-field-label">Mark</div>
+            <div className="dx-field-value">
+              <NumberBox
+                min={1}
+                max={6}
+                showSpinButtons
+                defaultValue={3}
+                value={markData.mark}
+                onValueChanged={({ value }) => setMarkData({ ...markData, mark: value })}
+              />
+            </div>
+          </div>
+          <div className="dx-field">
+            <div className="dx-field-label">Weight</div>
+            <div className="dx-field-value">
+              <RadioGroup
+                value={markData.weight}
+                items={tests}
+                valueExpr="id"
+                layout="horizontal"
+                onValueChanged={({ value }) =>
+                  setMarkData({ ...markData, weight: value.id ? value.id : value })
+                }
+              />
+            </div>
+          </div>
+          <div className="dx-field">
+            <div className="dx-field-label">Comment</div>
+            <div className="dx-field-value">
+              <TextArea
+                height={180}
+                value={markData.comment}
+                onValueChanged={({ value }) => setMarkData({ ...markData, comment: value })}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </Popup>
+  );
+}
