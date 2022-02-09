03cba7212f12455bd765288812bd015f0ae07ab7
Piotr Matras <piotrmatras21@gmail.com>
2022-02-09 23:44:39 +0100

    adjusted ThrottlerGuard to run properly behind proxy

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,9 +1,9 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiProperty } from '@nestjs/swagger';
-import { ThrottlerGuard } from '@nestjs/throttler';
 import { AuthService } from './auth/auth.service';
 import { Public } from './auth/decorators/public.decorator';
 import { LocalAuthGuard } from './auth/guards/local.guard';
+import { ThrottlerBehindProxyGuard } from './throttler.guard';
 
 export class LoginDto {
   @ApiProperty()
@@ -19,7 +19,7 @@ export class AppController {
   constructor(private authService: AuthService) {}
 
   @Public()
-  @UseGuards(ThrottlerGuard, LocalAuthGuard)
+  @UseGuards(ThrottlerBehindProxyGuard, LocalAuthGuard)
   @Post('login')
   async login(@Request() req) {
     return await this.authService.login(req.user);
