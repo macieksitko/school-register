62f214840d7c19e394d0c555fc4a79774b99ce16
Piotr Matras <piotrmatras21@gmail.com>
2022-02-09 23:52:52 +0100

    getting last IP from X-FORWARDED-FOR header

--- backend/src/throttler.guard.ts
+++ backend/src/throttler.guard.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class ThrottlerBehindProxyGuard extends ThrottlerGuard {
   protected getTracker(req: Record<string, any>): string {
-    return req.ips.length ? req.ips[0] : req.ip;
+    return req.ips.length ? req.ips[req.ips.length - 1] : req.ip;
   }
 }
